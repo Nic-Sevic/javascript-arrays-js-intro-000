881f2e737d9c700f2e2eb39451582ad83a6f434f
Nic Sevic <nasevic@gmail.com>
2019-03-16 22:48:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element){
-var elem = element
+var elem = [element]
   return [...elem, array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
